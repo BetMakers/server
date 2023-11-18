7ad862e81a40736a72fe48b326d8e6ce7a5e8b32
shafu <shafu0x@gmail.com>
2023-11-18 17:00:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Sample data
+tasks = [
+    {
+        'id': 1,
+        'title': 'Task 1',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Task 2',
+        'done': False
+    }
+]
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+# Route to get all tasks
+@app.route('/games', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
